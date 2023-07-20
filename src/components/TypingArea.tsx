4a25dca9c6c1a6f4608e4fb5d167e15e26ac998f
therealnv6 <devrawr@protonmail.com>
2023-07-20 17:36:36 +0200

    add caret thing

--- src/components/TypingArea.tsx
+++ src/components/TypingArea.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface TypingAreaProps {
   text: string;
   typed: string;
@@ -7,15 +9,19 @@ export const TypingArea: React.FC<TypingAreaProps> = ({ text, typed }) => {
   return (
     <h2 className="text-3xl font-normal transition-all">
       {text.split("").map((char, index) => (
-        <span
-          key={index}
-          className={`${index > typed.length - 1
-              ? "text-gray-500"
-              : (char === typed[index] ? "text-green-500" : "text-red-500")
-            }`}
-        >
-          {char}
-        </span>
+        <React.Fragment key={index}>
+          {index === typed.length && <span className="caret">&nbsp;</span>}
+          <span
+            className={`${index > typed.length - 1
+                ? "text-gray-500"
+                : char === typed[index]
+                  ? "text-green-500"
+                  : "text-red-500"
+              }`}
+          >
+            {char}
+          </span>
+        </React.Fragment>
       ))}
     </h2>
   );
